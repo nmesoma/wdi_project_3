364ce51ee0f4b2547c43521618bb2ada4c9d9a6b
nmesoma <christalmails@google.com>
2015-05-27 15:11:50 +0100

    review model created

---
+++ app/views/reviews/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @review, :id, :user_id, :product_id, :brand_id, :review_date, :created_at, :updated_at
---
+++ db/migrate/20150527141029_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer :user_id
+      t.integer :product_id
+      t.integer :brand_id
+      t.text :review_date
+
+      t.timestamps null: false
+    end
+  end
+end
