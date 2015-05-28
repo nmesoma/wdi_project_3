51fb9284e469bce3514d238ce387dca0197eeba6
nmesoma <christalmails@google.com>
2015-05-28 18:34:01 +0100

    Resolving routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :brands
   root 'products#index'
+  resources :brands
   resources :products
+  resources :locations
   resources :reviews
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   resources :users, only: [:index, :show]
